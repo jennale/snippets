e3c6b71f10fad26344ec97891e9b084247a3cf27
Jenna Le <jle249@uwo.ca>
2014-11-06 00:37:08 -0500

    Cleanup
    Added description

--- parseYTid.php
+++ parseYTid.php
@@ -1,23 +1,11 @@
 <?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
 
-$urls = array("http://youtu.be/0Bmhjf0rKe8",
-	"http//www.youtube.com/embed/0Bmhjf0rKe8",
-	"http//www.youtube.com/watch?v=0Bmhjf0rKe8",
-	"http//www.youtube.com/?v=0Bmhjf0rKe8",
-	"http//www.youtube.com/v/0Bmhjf0rKe8",
-	"http//www.youtube.com/e/0Bmhjf0rKe8",
-	"http//www.youtube.com/user/username#p/u/11/0Bmhjf0rKe8",
-	"http//www.youtube.com/sandalsResorts#p/c/54B8C800269D7C1B/0/0Bmhjf0rKe8BLAHHHHH",
-	"http//www.youtube.com/watch?feature=player_embedded&v=0Bmhjf0rKe8",
-	"http//www.youtube.com/?feature=player_embedded&v=0Bmhjf0rKe8");
-
-foreach ($urls as $i => $value) {
-    getYoutubeID($value);
-    echo "<br/>".$i;
-}
+/** PHP Youtube ID grabber
+ * Created to grab the Youtube video ID from a URL of various formats, using Regex and Php parsers.
+ * 
+ * Includes test cases with output.  
+ * jle
+ **/
 
 function getYoutubeId($url){
 	if (!strrpos($url,"v=")===false) {
@@ -32,18 +20,40 @@ function getYoutubeId($url){
 
 function checkYoutubeId($youtubeId){
 	if (strlen($youtubeId)==0){
-		echo 'no';
+		echo 'ID is invalid';
 		return false;
 		}
 	else {
 		$url = "http://gdata.youtube.com/feeds/api/videos/".$youtubeId;
-		$check = get_headers($url, 0);
+		$check = get_headers($url, 0); //Check if valid ID via HTTP request of gdata site.
 		if (!preg_match('/200/',(string)$check[0])===false){
 			echo $youtubeId;
 			return $youtubeId;
 		}
 		else 
-			checkYoutubeID(substr($youtubeId, 0, -1));
+			checkYoutubeID(substr($youtubeId, 0, -1)); //Deals with jargon at the end of URLs
 	}
-	
 }
+
+// Main // Test Cases
+$urls = array("http://youtu.be/0Bmhjf0rKe8",
+	"http//www.youtube.com/embed/0Bmhjf0rKe8",
+	"http//www.youtube.com/watch?v=0Bmhjf0rKe8",
+	"http//www.youtube.com/?v=0Bmhjf0rKe8",
+	"http//www.youtube.com/v/0Bmhjf0rKe8",
+	"http//www.youtube.com/e/0Bmhjf0rKe8",
+	"http//www.youtube.com/user/username#p/u/11/0Bmhjf0rKe8",
+	"http//www.youtube.com/sandalsResorts#p/c/54B8C800269D7C1B/0/0Bmhjf0rKe8BLAHHHHH",
+	"http//www.youtube.com/watch?feature=player_embedded&v=0Bmhjf0rKe8",
+	"http//www.youtube.com/?feature=player_embedded&v=0Bmhjf0rKe8");
+foreach ($urls as $i => $value) {
+    getYoutubeID($value);
+    echo "<br/>";
+}
+
+
+/** 
+ * ID is then used to interact with the Youtube API to grab the video's metadata. 
+ *	
+ * http://gdata.youtube.com/feeds/api/videos/--> ID HERE <--?v=2&prettyprint=true&alt=jsonc
+ **/
